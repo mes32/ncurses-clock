ffd47665fa1951e366df85cef31b55a01bec5d1c
mes32 <mes32@users.noreply.github.com>
2016-10-27 01:05:41 -0400

    feature: Display system time and update continuously

--- src/main.c
+++ src/main.c
@@ -1,21 +1,39 @@
 #include <ncurses.h>
-#include <string.h> 
+#include <string.h>
+#include <time.h>
+
+void loopWait();
 
 int main() {
 
-    char mesg[] = "01:19 AM";
-    int row, col;
-
     initscr();
-    getmaxyx(stdscr, row, col);
 
-    mvprintw(row / 2, (col-strlen(mesg)) / 2, "%s", mesg);
-    mvprintw(row - 1, 0, ":");
-    move(row - 1, 1);
+    while(1) {
+        time_t now = time(0);
+        char *mesg = ctime(&now);
 
-    refresh();
-    getch();
+        int row, col;
+        getmaxyx(stdscr, row, col);
+
+        mvprintw(row / 2, (col-strlen(mesg)) / 2, "%s", mesg);
+        mvprintw(row - 1, 0, ":");
+        move(row - 1, 1);
+
+        refresh();
+        loopWait();
+    }
+
     endwin();
 
     return 0;
+}
+
+void loopWait() {
+
+    long milliseconds = 50;
+
+    struct timespec ts;
+    ts.tv_sec = milliseconds / 1000;
+    ts.tv_nsec = (milliseconds % 1000) * 1000000;
+    nanosleep(&ts, NULL);
 }
