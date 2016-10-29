f7ce6754bbfc78d17c54ff1162054cfe1139e4f3
mes32 <mes32@users.noreply.github.com>
2016-10-29 00:47:02 -0400

    feature: Display clock text in color

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <time.h>
 
+
 static const long WAIT_MSEC = 50;
 static struct timespec WAIT_TIMESPEC;
 
@@ -12,6 +13,7 @@ static const size_t BUFFER_SIZE = 50;
 static int row, col;
 
 
+static void initializeNCScreen();
 static void initializeWait();
 static void waitInLoop();
 static void setTimeString(char *timeBuffer);
@@ -21,13 +23,18 @@ static void printDate(char *currentDate);
 static void printFooter();
 static void cursorToRestPosition();
 
+static const enum ColorPairType {
+    COLOR_PAIR_WHITE = 0,
+    COLOR_PAIR_GREEN = 1
+} ColorPairs;
+
 int main() {
 
     char timeBuffer[BUFFER_SIZE];
     char dateBuffer[BUFFER_SIZE];        
 
+    initializeNCScreen();
     initializeWait();
-    initscr();
 
     while(1) {
 
@@ -48,6 +55,19 @@ int main() {
     endwin();
 
     return 0;
+}
+
+
+static void initializeNCScreen() {
+    WINDOW *window = initscr();
+    start_color();
+
+    //bool hasColors = has_colors();
+    //bool canChangeColors = can_change_color();
+
+    init_pair(COLOR_PAIR_WHITE, COLOR_WHITE, COLOR_BLACK);
+    init_pair(COLOR_PAIR_GREEN, COLOR_GREEN, COLOR_BLACK);
+    wbkgd(window, COLOR_PAIR(COLOR_PAIR_WHITE));
 }
 
 static void initializeWait() {
@@ -77,14 +97,20 @@ static void setDateString(char *dateBuffer) {
 }
 
 static void printTime(char *currentTime) {
+    attron(COLOR_PAIR(COLOR_PAIR_GREEN));
     mvprintw(row / 2, (col - strlen(currentTime)) / 2, "%s", currentTime);
+    attroff(COLOR_PAIR(COLOR_PAIR_GREEN));
 }
 
 static void printDate(char *currentDate) {
+    attron(COLOR_PAIR(COLOR_PAIR_WHITE));
     mvprintw(row / 2 + 1, (col - strlen(currentDate)) / 2, "%s", currentDate);
+    attroff(COLOR_PAIR(COLOR_PAIR_WHITE));
 }
 static void printFooter() {
+    attron(COLOR_PAIR(COLOR_PAIR_GREEN));
     mvprintw(row - 1, 0, ":");
+    attroff(COLOR_PAIR(COLOR_PAIR_GREEN));
 }
 
 static void cursorToRestPosition() {
